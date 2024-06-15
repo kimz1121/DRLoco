be082a56d6bc07c7c5232dfa1f4612f70be4444e
kimz1121 <kimz1121@naver.com>
2024-06-16 04:23:21 +0900

    0615 정리 11

--- drloco/mcp_transfer_dir_list_copy.bash
+++ drloco/mcp_transfer_dir_list_copy.bash
@@ -1,10 +1,12 @@
+#$1 첫번째 파라미터를 의미
+
 robot_ws_path="$HOME/robot_learn"
-log_primitive_dir="logs_0613_final_only/mcp_dir_list/seed1/models"
-log_transfer_path="DRLoco/logs_0614_transfer"
-id="mcp_transfer_dir_list_to_all_00_tests_copy"
+log_primitive_dir="DRLoco/logs_0615_transfer/NewRandomGoalAnt-v2/PPO_mcp_transfer_dir_list_to_all_00/seed0/models"
+log_transfer_dir="DRLoco/logs_$1"
+id="mcp_transfer_dir_list_to_all_00"
 cd $robot_ws_path
 ptrhon_script="train_edit_06_mcp_naive_transfer_dir_all.py"
 echo "now running $ptrhon_script"
-python ./DRLoco/drloco/$ptrhon_script --vec_normalise true --id $id --seed 0 --logdir_primitive $log_primitive_dir --logdir_transfer $log_transfer_path --eval_freq 1000000
+python ./DRLoco/drloco/$ptrhon_script --vec_normalise true --id $id --seed 0 --logdir_primitive $log_primitive_dir --logdir_transfer $log_transfer_dir 
 
 
