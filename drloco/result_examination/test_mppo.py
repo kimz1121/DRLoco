5be66b5a106c861e78d7f5cb2fa03bad59aa63cf
kimz1121 <kimz1121@naver.com>
2024-06-15 23:46:15 +0900

    0615 정리 04

--- drloco/result_examination/test_mppo.py
+++ drloco/result_examination/test_mppo.py
@@ -18,15 +18,20 @@ from drloco.custom.policies_mcp import MCPNaive, MPPO
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--direction", type=int, default=0)
+parser.add_argument("--id", type=str)
+parser.add_argument("--checkpoint", type=str, default="final")
+
 args = parser.parse_args()
 
 direction = args.direction
-run_id = "PPO_mcp_transfer_dir_determine_45_to_000"
+run_id = args.id
+checkpoint = args.checkpoint
 save_gif = True
 eval_env = make_vec_env(ant_env.GoalAnt, n_envs=1, env_kwargs={'direction': direction})
-model_path = f'logs_0615_transfer/NewRandomGoalAnt-v2/{run_id}/seed0/models/final.zip'
+# 차후 다시 학습한 것으로 수정하기
+model_path = f'logs_0615_transfer_01/NewRandomGoalAnt-v2/{run_id}/seed0/models/NewRandomGoalAnt-v2/{run_id}_{checkpoint}_steps.zip'
 model = PPO.load(model_path, env=eval_env)
-save_path = "mcppo_{}".format(run_id)
+save_path = "evaluation_{}".format(run_id)
 os.makedirs(save_path, exist_ok=True)
 
 obs = eval_env.reset()
@@ -38,21 +43,20 @@ weights=[]
 print(f"Begin Evaluation")
 ep_max_len = 1000
 pbar = tqdm(total=ep_max_len)
-i = 0
 
 # ! Cardinal Direction Heading
-dirs = np.array([0, 45, 90, 135, 180, 225, 270, 315])
-dirs = np.array([0, 90, 180, 270])
-dirs = dirs[..., None]
-dirs = np.repeat(dirs, ep_max_len/dirs.shape[0], 1)
-dirs = dirs.flatten()
-
+# dirs = np.array([0, 45, 90, 135, 180, 225, 270, 315])
+# dirs = np.array([0, 90, 180, 270])
+# dirs = dirs[..., None]
+# dirs = np.repeat(dirs, ep_max_len/dirs.shape[0], 1)
+# dirs = dirs.flatten()
+# print(dirs)
 # ! S Shape Heading
 # dirs = np.concatenate((np.linspace(0, 180, ep_max_len // 2), np.linspace(180, 0, ep_max_len // 2)))
 # dirs = np.round(dirs, 1)
-
+i = 0
 while not done:
-    eval_env.envs[0].set_direction(dirs[i])
+    eval_env.envs[0].set_direction(direction)
     action, _ = model.predict(obs, deterministic=True)
     weight = model.policy.predict_weights(obs)
     weights.append(weight)
